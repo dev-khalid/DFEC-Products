99992cf3e10420f88db66c9d5b0665812616b6e9
dev-khalid <khalid.hoss.akash@gmail.com>
2024-09-08 11:15:57 +0600

    feat: setting up server for development - docker

---
+++ inventory/inventory-server/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT, () => {
+    console.log(
+      `Server is running in ${process.env.NODE_ENV} mode, on port: ${process.env.PORT}`,
+    );
+  });
+}
+bootstrap();
